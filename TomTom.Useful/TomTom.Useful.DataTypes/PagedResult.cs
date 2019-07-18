90b57c3dd9197b02a18668fdc9ad043ac67d36a4
soso tughushi <soso.tughushi@lykke.com>
2019-07-19 02:49:27 +0400

    Created Result and PagedResult classes.

---
+++ TomTom.Useful/TomTom.Useful.DataTypes/PagedResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TomTom.Useful.DataTypes
+{
+    public class PagedResult<T>
+    {
+        public PagedResult(IEnumerable<T> data, int offset, int totalSize)
+        {
+            Offset = offset;
+            TotalSize = totalSize;
+            Data = data;
+        }
+
+        public int Offset { get; }
+
+        public int TotalSize { get; }
+
+        public IEnumerable<T> Data { get; }
+
+        public PagedResult<TNew> Convert<TNew>(Func<T, TNew> selector)
+        {
+            return new PagedResult<TNew>(this.Data.Select(selector), this.Offset, this.TotalSize);
+        }
+    }
+}
---
+++ TomTom.Useful/TomTom.Useful.DataTypes/Result.cs
@@ -0,0 +1,35 @@
+﻿namespace TomTom.Useful.DataTypes
+{
+    public abstract class Result<TError>
+    {
+        protected Result()
+        {
+            this.IsSuccess = true;
+        }
+
+        protected Result(TError error)
+        {
+            Error = error;
+            this.IsSuccess = false;
+        }
+
+        public bool IsSuccess { get; }
+
+        public TError Error { get; }
+    }
+
+    public abstract class Result<T, TError> : Result<TError>
+    {
+        protected Result(T result) : base()
+        {
+            this.Value = result;
+        }
+
+        public Result(TError error) : base(error)
+        {
+
+        }
+
+        public T Value { get; }
+    }
+}
