97fa55a8cc663a63324ac175e62531138c92add9
SosoTughushi <mryemama@gmail.com>
2022-01-18 02:01:12 +0400

    Created TomTom.Userful.CQRS project.

---
+++ TomTom.Useful/TomTom.Useful.CQRS/ICommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using TomTom.Useful.DataTypes;
+
+namespace TomTom.Useful.CQRS
+{
+    public interface ICommandHandler<TCommand, TResponse> 
+        where TCommand : ICommand
+        where TResponse : Result
+    {
+        Task<TResponse> Handle(TCommand request, CancellationToken cancellationToken);
+    }
+}
---
+++ TomTom.Useful/TomTom.Useful.CQRS/ICommandPublisher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomTom.Useful.Messaging;
+
+namespace TomTom.Useful.CQRS
+{
+    public interface ICommandPublisher <TCommand> : IPublisher<TCommand>
+        where TCommand : ICommand
+    {
+    }
+}
