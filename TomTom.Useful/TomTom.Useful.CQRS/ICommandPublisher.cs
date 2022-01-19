f89bdd4b57f00bb53f0ed6dcce9e292e8dec6ba8
SosoTughushi <mryemama@gmail.com>
2022-01-19 19:15:35 +0400

    Created AsCommandPublisher and AsEventPublisher extension methods.

--- TomTom.Useful/TomTom.Useful.CQRS/ICommandPublisher.cs
+++ TomTom.Useful/TomTom.Useful.CQRS/ICommandPublisher.cs
@@ -11,4 +11,34 @@ namespace TomTom.Useful.CQRS
         where TCommand : ICommand
     {
     }
+
+    public static class CommandPublisherExtensions
+    {
+        public static ICommandPublisher<TCommand> AsCommandPublisher<TCommand>(this IPublisher<TCommand> publisher)
+            where TCommand: ICommand
+        {
+            return new CommandPublisherAdapter<TCommand>(publisher);
+        }
+
+        private class CommandPublisherAdapter<TCommand> : ICommandPublisher<TCommand>
+            where TCommand: ICommand
+        {
+            private readonly IPublisher<TCommand> publisher;
+
+            public CommandPublisherAdapter(IPublisher<TCommand> publisher)
+            {
+                this.publisher = publisher;
+            }
+
+            public Task Publish(TCommand message)
+            {
+                return this.publisher.Publish(message);
+            }
+
+            public Task Publish(IEnumerable<TCommand> messages)
+            {
+                return this.publisher.Publish(messages);
+            }
+        }
+    }
 }
--- TomTom.Useful/TomTom.Useful.EventSourcing/IEventPublisher.cs
+++ TomTom.Useful/TomTom.Useful.EventSourcing/IEventPublisher.cs
@@ -10,4 +10,31 @@ namespace TomTom.Useful.EventSourcing
     public interface IEventPublisher : IPublisher<Event>
     {
     }
+
+    public static class EventPublisherExtensions
+    {
+        public static IEventPublisher AsEventPublisher(this IPublisher<Event> publisher)
+        {
+            return new EventPublisherAdapter(publisher);
+        }
+
+        private class EventPublisherAdapter : IEventPublisher
+        {
+            private readonly IPublisher<Event> publisher;
+
+            public EventPublisherAdapter(IPublisher<Event> publisher)
+            {
+                this.publisher = publisher;
+            }
+            public Task Publish(Event message)
+            {
+                return publisher.Publish(message);
+            }
+
+            public Task Publish(IEnumerable<Event> messages)
+            {
+                return publisher.Publish(messages);
+            }
+        }
+    }
 }
