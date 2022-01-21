1d947b663e1348b031f496f5c85013b3abf792df
SosoTughushi <mryemama@gmail.com>
2022-01-21 18:58:05 +0400

    Created Demo.Domain.Commands project.

---
+++ TomTom.Useful/Demo/TomTom.Useful.Demo.Domain.Commands/Playlist/CreatePlaylistCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomTom.Useful.CQRS;
+using TomTom.Useful.Demo.Domain.Identities;
+
+namespace TomTom.Useful.Demo.Domain.Commands.Playlist
+{
+    public class CreatePlaylistCommand : CreateCommandBase<PlaylistIdentity>
+    {
+        public CreatePlaylistCommand(
+            Guid id, 
+            ExplorerIdentity explorerId,
+            DateTime creationTimestamp,
+            string title,
+            string causedById ,
+            string correlationId
+            ) : base(id, causedById, correlationId)
+        {
+            ExplorerId = explorerId;
+            CreationTimestamp = creationTimestamp;
+            Title = title;
+        }
+
+        public ExplorerIdentity ExplorerId { get; set; }
+        public DateTime CreationTimestamp { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class AddSongToPlaylistCommand : CommandBase<PlaylistIdentity>
+    {
+
+    }
+}
---
+++ TomTom.Useful/Demo/TomTom.Useful.Demo.Domain.Commands/Playlist/PublishPlaylistCommand.cs
@@ -0,0 +1,18 @@
+﻿using TomTom.Useful.CQRS;
+using TomTom.Useful.Demo.Domain.Identities;
+
+namespace TomTom.Useful.Demo.Domain.Commands.Playlist
+{
+    public class PublishPlaylistCommand : CommandBase<PlaylistIdentity>
+    {
+        public PublishPlaylistCommand()
+        {
+
+        }
+
+        public PublishPlaylistCommand(PlaylistIdentity targetIdentity, Guid id, string causedById, string correlationId) 
+            : base(targetIdentity, id, causedById, correlationId)
+        {
+        }
+    }
+}
