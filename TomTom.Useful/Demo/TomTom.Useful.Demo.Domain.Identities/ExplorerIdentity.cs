5d2876110ca5587d2fc4dc3d0925a71627b90ca8
SosoTughushi <mryemama@gmail.com>
2022-01-21 18:57:43 +0400

    Created Demo.Domain.Identities project.

---
+++ TomTom.Useful/Demo/TomTom.Useful.Demo.Domain.Identities/ExplorerIdentity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomTom.Useful.Demo.Domain.Identities
+{
+    public struct ExplorerIdentity : IEquatable<ExplorerIdentity>, IComparable<ExplorerIdentity>
+    {
+        public ExplorerIdentity(Guid value)
+        {
+            Value = value;
+        }
+
+        public Guid Value { get; }
+
+        public int CompareTo(ExplorerIdentity other)
+        {
+            return Value.CompareTo(other.Value);
+        }
+
+        public bool Equals(ExplorerIdentity other)
+        {
+            return this.Value.Equals(other.Value);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Value.GetHashCode();
+        }
+
+        public static implicit operator Guid(ExplorerIdentity err) => err.Value;
+
+        public static implicit operator ExplorerIdentity(Guid err) => new(err);
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (obj is not ExplorerIdentity) return false;
+            return Equals((ExplorerIdentity)obj);
+        }
+
+        public static bool operator ==(ExplorerIdentity left, ExplorerIdentity right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(ExplorerIdentity left, ExplorerIdentity right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ TomTom.Useful/Demo/TomTom.Useful.Demo.Domain.Identities/PlaylistIdentity.cs
@@ -0,0 +1,49 @@
+﻿namespace TomTom.Useful.Demo.Domain.Identities
+{
+    public struct PlaylistIdentity : IEquatable<PlaylistIdentity>, IComparable<PlaylistIdentity>
+    {
+        public PlaylistIdentity(Guid value)
+        {
+            Value = value;
+        }
+
+        public Guid Value { get; }
+
+        public int CompareTo(PlaylistIdentity other)
+        {
+            return Value.CompareTo(other.Value);
+        }
+
+        public bool Equals(PlaylistIdentity other)
+        {
+            return this.Value.Equals(other.Value);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (obj is not PlaylistIdentity) return false;
+            return Equals((PlaylistIdentity)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Value.GetHashCode();
+        }
+
+        public static implicit operator Guid(PlaylistIdentity err) => err.Value;
+
+        public static implicit operator PlaylistIdentity(Guid err) => new(err);
+
+
+        public static bool operator ==(PlaylistIdentity left, PlaylistIdentity right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(PlaylistIdentity left, PlaylistIdentity right)
+        {
+            return !(left == right);
+        }
+    }
+}
